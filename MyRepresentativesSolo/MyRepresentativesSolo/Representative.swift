88ab5293b3971a2da8a01e828d5ea61e1f2f06b7
Austin West <korruptedanomaly@gmail.com>
2019-05-16 17:09:49 -0600

    Update before leave

---
+++ MyRepresentativesSolo/MyRepresentativesSolo/Representative.swift
@@ -0,0 +1,20 @@
+//
+//  Representative.swift
+//  MyRepresentativesSolo
+//
+//  Created by Austin West on 5/16/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+import Foundation
+
+struct Representative: Codable {
+    
+    let name: String
+    let party: String
+    let state: String
+    let district: String
+    let phone: String
+    let office: String
+    let link: String
+}
